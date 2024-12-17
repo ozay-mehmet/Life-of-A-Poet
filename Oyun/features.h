b57cc903b8f5cbedc3fdadbcffffca81b34e5dd8
Mehmet Özay <137615002+ozay-mehmet@users.noreply.github.com>
2024-12-17 20:04:44 +0300

    oyun klasörü yüklendi.

---
+++ Oyun/features.h
@@ -0,0 +1,29 @@
+#ifndef FEATURES_H
+#define FEATURES_H
+
+extern MAX_FEATURE_VALUE_QUALITY;
+extern MIN_FEATURE_VALUE_QUALITY;
+extern MAX_FEATURE_VALUE_SKILLS;
+extern MIN_FEATURE_VALUE_SKILLS;
+
+extern float SANS;
+extern int PUAN;
+extern int CAN;
+extern int TOK;
+extern int HIJYEN;
+extern int MUTLULUK;
+extern int SAGLIK;
+extern int ALTIN;
+extern int GUC;
+extern int KARIZMA;
+extern int CEVIKLIK;
+extern int DAYANIKLILIK;
+extern int BINICILIK;
+extern int AVCILIK;
+extern int TOPLAYICILIK;
+
+void setFeatureValue(int *feature, int value);
+void showQualification();
+void showSkills();
+
+#endif
